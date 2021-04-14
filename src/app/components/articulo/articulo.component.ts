2eb097a835f744ab86a452fbc2fb5335c9a10026
JohnGranados01 <jhongranadossalamanca@gmail.com>
2021-04-14 12:04:42 -0500

    Actualizar articulo.ts, articulo.ts y 7 más archivos...

--- src/app/components/articulo/articulo.component.ts
+++ src/app/components/articulo/articulo.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter} from '@angular/core';
+import { Articulo } from 'src/app/Modelo/articulo';
 
 @Component({
   selector: 'app-articulo',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticuloComponent implements OnInit {
 
+  /* recibiendo un parametro
+  Propiedades de entrada y salida */
+
+  //recibe un objeto
+
+  //evento
+  @Input() articulo: Articulo = new Articulo();
+  @Output() deleteArticulo: EventEmitter<Articulo> = new EventEmitter();
+
+
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onDelete(articulo: Articulo){
+    this.deleteArticulo.emit(articulo);
+  }
+
+  onToggle(articulo: Articulo){
+    articulo.pago = !articulo.pago;
   }
 
 }
