6efb0090c74a3b92503e4d8862225cf6d194cf56
Jaac25 <acevedoalexander52@gmail.com>
2021-04-15 17:57:27 -0500

    Final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,11 @@ const routes: Routes = [
     path: 'inicio', loadChildren: () => 
     import('./components/mostrar/mostrar.module').then(m => m.MostrarModule)
   },
-  
   {path:'',pathMatch: 'full', redirectTo: 'inicio'}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/components/mostrar/mostrar-routing.module.ts
+++ src/app/components/mostrar/mostrar-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MostrarComponent } from './mostrar.component';
+import { MostrarModule } from './mostrar.module';
 
 const routes: Routes = [
   {path: '',component: MostrarComponent}
